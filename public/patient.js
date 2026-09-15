caadf1a1ea79970e13651e65a8ce820182119749
Aditya Pattnaik <adityapattnaik393@gmail.com>
2026-09-15 05:00:31 +0000

    patient dashboard update

--- public/patient.js
+++ public/patient.js
@@ -96,7 +96,7 @@
 
     var hour = new Date().getHours();
     var greetKey = hour < 12 ? "morning" : hour < 18 ? "afternoon" : "evening";
-    MB.setText("greetTime", t(greetKey) + ", " + t("caretakerSuffix"));
+    MB.setText("greetTime", t(greetKey) + ", patient " + state.profile.patientName);
   }
 
   function renderProfile(profile) {
